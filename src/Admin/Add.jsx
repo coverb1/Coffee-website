9f3cfffc09ca170b85a10e62c602f7a2e16c5e16
Mucyobruce <cboy85096@email.com>
2025-12-29 11:43:55 +0200

    changes

--- src/Admin/Add.jsx
+++ src/Admin/Add.jsx
@@ -1,36 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets } from '../assets/assets'
 
 const Add = () => {
+
+const [image1,setImage1]=useState(false)
+const [image2,setImage2]=useState(false)
+const [image3,setImage3]=useState(false)
+const [image4,setImage4]=useState(false)
+const [image5,setImage5]=useState(false)
+
+const[name,setName]=useState("")
+const[description,setDescription]=useState("")
+const[price,setPrice]=useState('')
+
   return (
     <form className='flex flex-col mt-10 ml-10 gap-5' >
       <div>
-        <p>Upload Image</p>
+        <p>Upload Images</p>
 
-        <div className=' flex gap-5'>
-          <label htmlFor="image1">
-            <img src={assets.uploading} alt="" className='w-10' />
-            <input type="file" id='image1' className='hidden' />
+        <div className=' flex gap-5 '>
+          <label htmlFor="image1 ">
+            <img src={image1?URL.createObjectURL(image1):assets.uploading} alt="" className='w-10 cursor-pointer'  />
+            <input type="file" onChange={(e)=>setImage1(e.target.files[0])} id='image1' className='hidden' />
           </label>
 
           <label htmlFor="image2">
-            <img src={assets.uploading} alt="" className='w-10' />
-            <input type="file" id='image2' className='hidden' />
+            <img src={image2?URL.createObjectURL:assets.uploading} alt="" className='w-10 cursor-pointer'  />
+            <input onChange={(e)=>setImage2(e.target.files[0])} type="file" id='image2' className='hidden' />
           </label>
 
           <label htmlFor="image3">
-            <img src={assets.uploading} alt="" className='w-10' />
-            <input type="file" id='image3' className='hidden' />
+            <img src={image3?URL.createObjectURL:assets.uploading} alt="" className='w-10 cursor-pointer' />
+            <input type="file" id='image3'  onChange={(e)=>setImage3(e.target.files[0])} className='hidden' />
           </label>
 
           <label htmlFor="image4">
-            <img src={assets.uploading} alt="" className='w-10' />
-            <input type="file" id='image4' className='hidden' />
+            <img src={image4?URL.createObjectURL:assets.uploading} alt="" className='w-10 cursor-pointer' />
+            <input type="file" onChange={(e)=>setImage4(e.target.files[0])} id='image4' className='hidden' />
           </label>
 
           <label htmlFor="image5">
-            <img src={assets.uploading} alt="" className='w-10' />
-            <input type="file" id='image5' className='hidden' />
+            <img src={image5?URL.createObjectURL:assets.uploading} alt="" className='w-10 cursor-pointer' />
+            <input type="file" onChange={(e)=>setImage5(e.target.files[0])} id='image5' className='hidden' />
           </label>
         </div>
       </div>
@@ -47,6 +58,12 @@ const Add = () => {
        <textarea name="" id="" placeholder='Write content here' className='border border-black outline-none
         text-base px-2 bg-white max-w-[300px]'>
         </textarea>
+
+         <div className='flex gap-2 flex-col w-full'>
+        <p>Product Price</p>
+        <input type="Number" placeholder='24' className='border border-black outline-none
+        text-base px-2 bg-white max-w-[300px]' />
+      </div>
       </div>
     </form>
   )
