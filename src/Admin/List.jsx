cd58b2ac6ac4b5ac820af80b927952e958ee1514
Mucyobruce <cboy85096@email.com>
2025-12-31 09:13:26 +0200

    changes

--- src/Admin/List.jsx
+++ src/Admin/List.jsx
@@ -3,25 +3,44 @@ import axios from 'axios'
 import { API_URL } from '../Loginpage/Loginpage'
 const List = () => {
 
-const [foods,setfoods]=useState([])
+  const [foods, setfoods] = useState([])
 
-const handlegetList=async()=>{
-  try {
-    const res=await axios.get(`${API_URL}/getfood`)
-    setfoods(res.data.products || [])
-    console.log("fopd gotten well",res)
-  } catch (error) {
-    console.log(error)
+  const handlegetList = async () => {
+    try {
+      const res = await axios.get(`${API_URL}/getfood`)
+      setfoods(res.data || [])
+      console.log("fopd gotten well", res)
+    } catch (error) {
+      console.log(error)
+    }
   }
-}
-useEffect(()=>{
-  handlegetList()
-},[])
+  useEffect(() => {
+    handlegetList()
+  }, [])
   return (
     <div>
-{foods.map((food,index)=>(
-  <p key={index}>{food.name}</p>
-))}
+      <div>
+        <p>All Products</p>
+      </div>
+
+      <div className='flex flex-col gap-2'>
+
+        <div className='hidden md:grid grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center py-1 px-2 border bg-gray-100 text-sm'>
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+
+      </div>
+
+      {foods.map((food, index) => (
+        <>
+          {/* <p key={index}>{food.name}</p> */}
+
+        </>
+      ))}
     </div>
   )
 }
