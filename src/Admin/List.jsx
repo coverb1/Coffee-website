33188e63298db5704878cb66a72d79e84a3600e1
Mucyobruce <cboy85096@email.com>
2026-01-02 12:54:55 +0200

    changes

--- src/Admin/List.jsx
+++ src/Admin/List.jsx
@@ -2,9 +2,22 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { API_URL } from '../Loginpage/Loginpage'
 import { assets } from '../assets/assets'
+import { toast } from 'react-toastify'
 const List = () => {
 
   const [foods, setfoods] = useState([])
+
+  const handleDeleteItem = async (id) => {
+    try {
+      const delelteItem = await axios.delete(`${API_URL}/AdmindeleteItem/${id}`)
+    setfoods(prev => prev.filter(i => i._id !== id))
+    toast.success('Item deleted well',delelteItem)
+    console.log('Item deleted well')
+    } catch (error) {
+      toast.error("Failed to delete")
+      console.log(error)
+    }
+  }
 
   const handlegetList = async () => {
     try {
@@ -37,11 +50,12 @@ const List = () => {
 
       {foods.map((food, index) => (
         <>
-        <div className='grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text-sm'>
-        <img src={food.image[0].url} alt="" className='w-11 h-11' />
-          <p>{food.name}</p>
-          <p>$:{food.price}</p>
-          <img src={assets.deleteIcon} alt="" className='w-6 h-6' />
+          <div className='grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text-sm'>
+            <img src={food.image[0].url} alt="" className='w-11 h-11' />
+            <p>{food.name}</p>
+            <p>$:{food.price}</p>
+            <img src={assets.deleteIcon} alt="" 
+           onClick={()=>handleDeleteItem(food._id)}   className='w-6 h-6 cursor-pointer' />
           </div>
         </>
       ))}
